29693c04f6dd273c2daa3304c21bf97ce81af93f
MTF-dasher <masayuki206@outlook.jp>
2021-08-06 08:58:24 +0000

    login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'session/new'
+  get 'sessions/new'
   get 'signup' => 'users#new'
   root 'static_pages#home'
   get 'help' => 'static_pages#help'
@@ -7,5 +9,8 @@ Rails.application.routes.draw do
   #resources :tweets
   #resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   resources :users
 end
